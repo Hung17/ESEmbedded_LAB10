b1e72bfff5dd23b02c0c1b7add3bfd1763e854e8
Hung17 <aaron11221004@gmail.com>
2019-06-05 16:14:32 +0800

    mpu

--- 2_mpu/asm_func.s
+++ 2_mpu/asm_func.s
@@ -2,24 +2,37 @@
 
 .global	read_sp
 read_sp:
-	??????
+	MOV r0, SP;
+	bx lr;
 
 .global	read_msp
 read_msp:
-	??????
+	MRS r0, MSP;
+	bx lr;
 
 .global	read_psp
 read_psp:
-	??????
+	MRS r0, PSP;
+	bx lr;
 
 .global	read_ctrl
 read_ctrl:
-	??????
+	MRS r0, CONTROL;
+	bx lr;
+
 
 .global	start_user
 start_user:
-	??????
+	MOV lr, r0;
+	MSR PSP, r1;
+	MOV r0, #0b11;
+	MSR CONTROL, r0;
+	ISB;
+	bx lr;
 
 .global	sw_priv
 sw_priv:
-	??????
+	MOV r3, #0;
+	MSR CONTROL, r3;
+	ISB;
+	bx lr;
